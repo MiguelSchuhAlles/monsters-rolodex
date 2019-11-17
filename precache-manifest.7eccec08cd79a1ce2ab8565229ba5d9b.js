6333ed45fe3538ac818a7d4e6bf9785cfb5b0d84
Miguel Alles <miguelschuhalles@gmail.com>
2019-11-17 17:49:46 -0300

    Updates

---
+++ precache-manifest.7eccec08cd79a1ce2ab8565229ba5d9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90dabe1bb9d124f02e264a9e9c8ee7ff",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1252bfba657769f935ae",
+    "url": "/monsters-rolodex/static/css/main.a9e98b59.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "1252bfba657769f935ae",
+    "url": "/monsters-rolodex/static/js/main.b7a4f2c2.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
